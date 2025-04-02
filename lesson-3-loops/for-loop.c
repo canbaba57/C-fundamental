78d44e8a01a948b93ec771ae8b1552a0a74b23af
Can Çetin <cancetin5762@gmail.com>
2025-04-02 14:49:07 +0300

    for_loop

--- lesson-3-loops/for-loop.c
+++ lesson-3-loops/for-loop.c
@@ -3,11 +3,12 @@
 int     main()
 {
         int count = 10;
-        for (int i = 0; i < count; i++) //İlk başta i değişkenini tanımlıyoruz ardından karşılaştırma yapıyoruz koşul sağlanırsa döngünün içindeki kod çalışır. Sonra for 
+        for (int i = 0; ++i < count;) //İlk başta i değişkenini tanımlıyoruz ardından karşılaştırma yapıyoruz koşul sağlanırsa döngünün içindeki kod çalışır. Sonra for 
         {//döngüsünün içindeki i++ komutu yani i 'yi 1 artırır. Sonra tekrardan kondisyonu kontrol eder i < count mu diye.
             /* code */
             printf("%d\n", i);
         }
         //Önemli not: for döngüsünün içindeki her tanımlama sonrası ; koyulur kesin kural. Mesela int i = 0 yazmak istemezseniz onu dışarda tanımlarsınız ama yinede
         // ; konması lazım başa !!!!!!!!!!!!!!!!
+        
 }
