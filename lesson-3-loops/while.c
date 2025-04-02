d9c03872be8a1fdfe11bd659f9eaa0d2824ee4c7
Can Çetin <cancetin5762@gmail.com>
2025-04-02 14:23:42 +0300

    while

---
+++ lesson-3-loops/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int     main()
+{
+        int i = 1;
+        
+        while(i <= 10)
+        {
+            printf("%d\n", i);
+            i++;//i'yi bir artır demek bu
+        }
+        return (0);
+}
